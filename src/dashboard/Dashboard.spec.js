edc175cab02613d517dbd3a670e151abd7083795
Kyle Baker <k@gnomiq.com>
2019-02-05 20:19:46 -0800

    should display proper text content

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -6,8 +6,8 @@ import { toHaveTextContent, toHaveAttribute } from 'jest-dom';
 expect.extend({ toHaveTextContent, toHaveAttribute });
 
 
-describe('Controls', () => {
-    it('cannot be closed or opened if it is locked', () => {
+describe('Dashboard', () => {
+    it('cannot be closed or opened if it is locked and should display proper text content', () => {
         const dashboard = render(<Dashboard/>);
         const openBtn = dashboard.getByTestId('openBtn');
         const lockBtn = dashboard.getByTestId('lockBtn');
@@ -15,7 +15,13 @@ describe('Controls', () => {
         const lockedState = dashboard.getByTestId('lockedState');
 
         fireEvent.click(openBtn); // Open the gate so lock button is enabled
+        expect(openState).toHaveTextContent(/closed/i);
         fireEvent.click(lockBtn); // Lock the gate to test if open button is disabled
+        expect(lockedState).toHaveTextContent(/locked/i);
         expect(openBtn).toHaveAttribute('disabled');
+        fireEvent.click(lockBtn);
+        expect(lockedState).toHaveTextContent(/unlocked/i);
+        fireEvent.click(openBtn);
+        expect(openState).toHaveTextContent(/open/i);
     });
 });
